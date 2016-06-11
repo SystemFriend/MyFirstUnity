e8dbeebed869bc9cb5d0b96c7e28ab577850fad5
satoshi-maemoto <peugeot.106.s16@gmail.com>
2016-06-11 17:13:10 +0900

    oops! Unity WebGL not support moblie browsers!

--- MyFirstUnity/Assets/Scripts/KeyMove.cs
+++ MyFirstUnity/Assets/Scripts/KeyMove.cs
@@ -12,9 +12,12 @@ public class KeyMove : MonoBehaviour {
         //現在の回転量を取得する
         var currentRotation = this.transform.rotation;
 
-        //左右の回転軸（左右矢印キーで動く）の値を取得する
+        //上下左右の回転軸（左右矢印キーで動く）の値を取得する
         //Quarternionは掛け算をすると回転量が変わる
-        var nextRotation = currentRotation * Quaternion.AngleAxis(Input.GetAxis("Horizontal"), Vector3.forward);
+        var horizontalRotation = Quaternion.AngleAxis(Input.GetAxis("Horizontal"), Vector3.forward);
+        var verticalRotation = Quaternion.AngleAxis(Input.GetAxis("Vertical"), Vector3.right);
+
+        var nextRotation = currentRotation * horizontalRotation * verticalRotation;
 
         //計算後の回転量を設定する
         this.transform.rotation = nextRotation;
