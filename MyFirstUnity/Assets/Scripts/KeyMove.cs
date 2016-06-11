bd9276a916c34c10fc978cc2693eb08f35a9c775
satoshi-maemoto <peugeot.106.s16@gmail.com>
2016-06-11 15:22:08 +0900

    implements sample.

---
+++ MyFirstUnity/Assets/Scripts/KeyMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class KeyMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //現在の回転量を取得する
+        var currentRotation = this.transform.rotation;
+
+        //左右の回転軸（左右矢印キーで動く）の値を取得する
+        //Quarternionは掛け算をすると回転量が変わる
+        var nextRotation = currentRotation * Quaternion.AngleAxis(Input.GetAxis("Horizontal"), Vector3.forward);
+
+        //計算後の回転量を設定する
+        this.transform.rotation = nextRotation;
+    }
+}
